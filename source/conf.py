9c999c41bd213c600c5a92e2dca8aeb14698baa6
ftnext <takuyafjp+develop@gmail.com>
2024-02-03 18:54:38 +0900

    [chore] sphinx-quickstart
    sphinx-quickstart --sep -p 2024_slides -a nikkie -l ja -r '' --ext-githubpages --no-batchfile --extensions sphinx_revealjs --
    extensions sphinx_design

---
+++ source/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = '2024_slides'
+copyright = '2024, nikkie'
+author = 'nikkie'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.githubpages',
+    'sphinx_revealjs',
+    'sphinx_design',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'ja'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ source/index.rst
@@ -0,0 +1,20 @@
+.. 2024_slides documentation master file, created by
+   sphinx-quickstart on Sat Feb  3 18:53:25 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to 2024_slides's documentation!
+=======================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
